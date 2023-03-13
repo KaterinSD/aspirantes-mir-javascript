b44dcfebe3b605c565bb417501d8f81fc6216623
Katerin <kasantanad@hotmail.com>
2023-03-12 22:52:24 -0500

    solucion a los ejercicios de js

---
+++ js-1/conditionals.js
@@ -0,0 +1,19 @@
+function calcularColor(color) {
+    var color;
+    if (color === 1) {
+        console.log("negro");
+    } else if (color === 2) {
+        console.log("blanco");
+    } else if (color === 3) {
+        console.log("rojo");
+    } else {
+        console.log("verde");
+    }
+
+    return "El color es " + "";
+};
+
+console.log(calcularColor(1))
+console.log(calcularColor(2))
+console.log(calcularColor(3))
+console.log(calcularColor(8))
---
+++ js-1/sum.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    return  a + b
+}
+
+console.log(sum(4, 6)) 
+console.log(sum(10, 45)) 
+console.log(sum(15, 105))
