20f5923fb3c222b3a89c8831530b81247b79bab9
Katerin <kasantanad@hotmail.com>
2023-03-20 01:00:38 -0500

    Añadir indentación a sum.js

--- js-1/sum.js
+++ js-1/sum.js
@@ -1,11 +1,11 @@
 function sum(numero) {
-    let suma =0;
-    for (let i = 1; i <= numero; i ++) {
-         suma += i;
+    let suma = 0;
+    for (let i = 1; i <= numero; i++) {
+        suma += i;
     }
     return suma;
 }
 
-console.log(sum(4)) 
-console.log(sum(10)) 
+console.log(sum(4))
+console.log(sum(10))
 console.log(sum(15))
